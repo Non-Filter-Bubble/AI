30de5b8d9b98faabf617ed63c3f29a764869b28d
jisoo <zlsoo9ee@gmail.com>
2024-05-27 13:00:54 +0900

    UPDATE main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import pandas as pd
-from sentence_transformers import SentenceTransformer, util
-from typing import List
 import requests
 
 app = FastAPI()
@@ -22,33 +18,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 데이터 로드
-df = pd.read_csv('last_book.csv')
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI!"}
 
-# Sentence Transformer 모델 로드
-embedder = SentenceTransformer("jhgan/ko-sroberta-multitask")
-
-class KeywordsRequest(BaseModel):
-    keywords: List[str]
-
-@app.post("/recommend")
-async def recommend(request: KeywordsRequest):
-    user_keywords = " ".join(request.keywords)
-    user_embedding = embedder.encode(user_keywords, convert_to_tensor=True)
-
-    # 모든 책에 대해 임베딩 계산
-    df['embedding'] = df['KEYWORD_oneline'].apply(lambda x: embedder.encode(x, convert_to_tensor=True))
-    # 코사인 유사도 계산
-    df['similarity'] = df['embedding'].apply(lambda x: util.pytorch_cos_sim(user_embedding, x).item())
-
-    # 유사도가 높은 상위 5권 추천
-    recommendations = df.sort_values(by='similarity', ascending=False).head(5)
-
-    # Spring 애플리케이션에 데이터 전송
-    spring_app_url = "http://43.203.38.124:8080/recommend"
-    response = requests.post(spring_app_url, json=recommendations[['ISBN_THIRTEEN_NO', 'GENRE_LV2', 'similarity']].to_dict(orient='records'))
+@app.get("/test")
+async def test():
+    # Spring 애플리케이션에 요청 보내기
+    spring_app_url = "http://43.203.38.124:8080/test"
+    response = requests.get(spring_app_url)
     
-    return response.json()
+    return {"response_from_spring": response.text}
 
 if __name__ == '__main__':
     import uvicorn
